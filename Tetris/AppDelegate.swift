a63ef62e9a17fb2625da7e1fed09808a2ab21f31
Thomas Loux <thomasloux@mbp-de-thomas.lan>
2016-01-26 18:37:38 -1000

    Initial Commit

---
+++ Tetris/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tetris
+//
+//  Created by Thomas Loux on 26/01/2016.
+//  Copyright © 2016 Thomas Loux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tetris/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tetris
+//
+//  Created by Thomas Loux on 26/01/2016.
+//  Copyright © 2016 Thomas Loux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
